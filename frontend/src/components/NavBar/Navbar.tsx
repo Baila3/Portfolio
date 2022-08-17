4d07c5f208302002432966218707ee4ff07dd110
Baila3 <bailaly16@gmail.com>
2022-08-16 23:02:32 -0400

    creation of logo and implementation

--- frontend/src/components/NavBar/Navbar.tsx
+++ frontend/src/components/NavBar/Navbar.tsx
@@ -11,19 +11,8 @@ export default function Navbar () {
   return (
     <main>
       <div className="nav-div">
-        <img className="NavMage" src={require('../../docs/Screen Shot 2022-08-14 at 3.54.46 PM.png')} alt="" width = "100" height = "auto"/>
+        <img className="NavMage" src={require('../../docs/Logo.png')} alt="" width = "100" height = "auto"/>
         <div className="options">
-          {/* <Dropdown>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-              Apps
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item href="/EAC">Action</Dropdown.Item>
-              <Dropdown.Item href="/Scheduler">Another action</Dropdown.Item>
-              <Dropdown.Item href="/WikiMaps">Something else</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown> */}
           <h4>About</h4>
         </div>
       </div>
