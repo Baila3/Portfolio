44c1f84f4ea41e1f3915bd4d886cdff6fedc5869
Baila3 <bailaly16@gmail.com>
2022-08-21 20:56:03 -0400

    add background vid and button for scheduler

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -10,13 +10,24 @@ export default function Home () {
 
   return (
     <main>
-        <section className="buttons">
+        <section className="EACbut">
           <button className="draw">EAC</button>
-        </section>       
+        </section> 
         <div>
           <video id="blur" autoPlay loop muted  width="100%" height= "auto">
             <source
               src={require("../../docs/EAC.mp4")}
+              type="video/mp4"
+            />
+          </video>
+        </div>
+        <section className="Scheduler">
+          <button className="draw2">Scheduler</button>
+        </section>        
+        <div>
+          <video id="blur" autoPlay loop muted  width="100%" height= "auto">
+            <source
+              src={require("../../docs/Scheduler.mp4")}
               type="video/mp4"
             />
           </video>
