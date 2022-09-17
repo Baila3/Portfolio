82cf561976199e780ad0176ccabb3d439b36a632
Baila3 <bailaly16@gmail.com>
2022-09-16 21:55:13 -0400

    transitioning logo

--- frontend/src/components/NavBar/Navbar.tsx
+++ frontend/src/components/NavBar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 
@@ -6,11 +6,19 @@ interface Props {
   Text: string
 }
 export default function Navbar () {
-
+  const [logo, setLogo] = useState(0)
+  const changeLogo = () => {
+    if (window.scrollY > 900) {
+      setLogo(1)
+    } else {
+      setLogo(0)
+    }
+  }
+  window.addEventListener('scroll', changeLogo)
   return (
     <main>
       <div className="nav-div">
-        <img className="NavMage" src={require('../../docs/Logo.png')} alt="" width = "85" height = "auto"/>
+        <img className="NavMage" src={logo ? require('../../docs/Logo2.png') : require('../../docs/Logo.png')} alt="" width = "85" height = "auto"/>
         <div className="options">
           <h2 className="EAC">EAC</h2>
           <h2 className="WikiMaps">WikiMaps</h2>
