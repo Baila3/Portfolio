2b6c7334788ee4251dbc61642104b4b798f8bcba
Baila3 <bailaly16@gmail.com>
2022-09-18 18:41:13 -0400

    create customized logos

--- frontend/src/components/NavBar/Navbar.tsx
+++ frontend/src/components/NavBar/Navbar.tsx
@@ -7,6 +7,7 @@ interface Props {
 }
 export default function Navbar () {
   const [logo, setLogo] = useState(0)
+  const [nav, setNav] = useState(0)
   const changeLogo = () => {
     if (window.scrollY > 900) {
       setLogo(1)
@@ -14,16 +15,26 @@ export default function Navbar () {
       setLogo(0)
     }
   }
+  const changeNav = () => {
+    if (window.scrollY > 900 && window.scrollY < 1800  ) {
+      setNav(1)
+    } else if (window.scrollY > 1800) {
+      setNav(2)
+    } else {
+      setNav(0)
+    }
+  }
   window.addEventListener('scroll', changeLogo)
+  window.addEventListener('scroll', changeNav)
   return (
     <main>
       <div className="nav-div">
-        <img className="NavMage" src={logo ? require('../../docs/Logo2.png') : require('../../docs/Logo.png')} alt="" width = "85" height = "auto"/>
+        <img className={nav ?  "navPos1" : "NavMage"} src={logo ? require('../../docs/Logo2.png') : require('../../docs/Logo.png')} alt="" width = "85" height = "auto"/>
         <div className="options">
-          <h2 className="EAC">EAC</h2>
-          <h2 className="WikiMaps">WikiMaps</h2>
-          <h2 className="Scheduler">Scheduler</h2>
-          <h2 className="About">About</h2>
+          <h2 className={nav ? "eacPos" : "EAC" }>EAC</h2>
+          <h2 className={nav ?  "eacPos" : "WikiMaps" }>WikiMaps</h2>
+          <h2 className={nav ?  "eacPos" : "Scheduler" }>Scheduler</h2>
+          <h2 className={nav ?  "eacPos" : "About" }>About</h2>
         </div>
       </div>
     </main>
